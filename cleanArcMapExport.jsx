ea1ded4b1668f35ef3e43c9e15e6a2f49e53a00f
Hugo Ahlenius <hugo.ahlenius@nordpil.com>
2014-12-15 12:46:33 +0100

    fixes

--- cleanArcMapExport.jsx
+++ cleanArcMapExport.jsx
@@ -1,49 +1,73 @@
 ﻿// Fix exports from ArcGIS
 /* jshint ignore:start */
 #target Illustrator-18.064
+#include '/c/Users/hugo/config/Application Data/illustrator_scripts/Extendables/extendables.jsx'
 #includepath (new File($.fileName)).parent 
 #include 'utility.jsx'
-/* global activeDocument,ZOrderMethod,ElementPlacement,dispAlert,ungroup */
+/* global $,activeDocument,ZOrderMethod,ElementPlacement,dispAlert,ungroup,getChildAll */
 /* jshint ignore:end */
 
 var lyrBound = activeDocument.layers.add();
-var newLayer;
+var thisLayer, lyrMain;
+var firstClip = true;
 
 lyrBound.name = "Frame guide";
-lyrBound.printable = false;
+lyrBound.printable = true;
 lyrBound.zOrder(ZOrderMethod.SENDTOBACK);
+lyrBound.printable = false;
 
-var lyrMain = activeDocument.layers.getByName('Layers');
+try {
+	lyrMain = activeDocument.layers.getByName('Layers');
+}
+catch(err) {
+	lyrMain = activeDocument.layers[0];
+}
 
-for (var intI=0; intI < lyrMain.layers.length; intI++) {
-	// $.writeln(lyrMain.layers[intI].name);
-	if (lyrMain.layers[intI].groupItems.length) {
-		newLayer = activeDocument.layers.add();
-		newLayer.name = lyrMain.layers[intI].name;
-		newLayer.move(lyrMain, ElementPlacement.PLACEBEFORE);
-		lyrMain.layers[intI].groupItems[0].move(newLayer,ElementPlacement.INSIDE);
-		ungroup(newLayer.groupItems[0]);
-	} else if (lyrMain.layers[intI].layers.length) {
-		while (lyrMain.layers[intI].layers.length) {
-			newLayer = activeDocument.layers.add();
-			newLayer.name = lyrMain.layers[intI].layers[0].name;
-			newLayer.move(lyrMain, ElementPlacement.PLACEBEFORE);
-			lyrMain.layers[intI].layers[0].groupItems[0].move(newLayer,ElementPlacement.INSIDE);
-			lyrMain.layers[intI].layers[0].remove();
-			ungroup(newLayer.groupItems[0]);
+function removeClip(thisLayer) {
+	var elements = getChildAll(thisLayer);
+	for (var j=0; j < elements.length; j++) {
+		if (elements[j].clipping) {
+			if (firstClip) {
+				// If this is the first layer, move the clipping path to the 'frame guide' layer, otherwise delete it
+				try {
+					elements[j].move(lyrBound,ElementPlacement.INSIDE);
+				}
+				catch (err) {
+					$.writeln("Failed to move clipping mask to frame layer");
+					$.writeln(err);
+				}
+				lyrBound.visible = false;
+				firstClip = false;
+			} else {
+				elements[j].remove();
+			}
 		}
 	}
+}
 
-	// Remove clipping path
-	for (var intJ=0; intJ < newLayer.pathItems.length; intJ++) {
-		if (newLayer.pathItems[intJ].clipping) {
-			if (intI === 0) {
-				// If this is the first layer, move the clipping path to the 'frame guide' layer, otherwise delete it
-				newLayer.pathItems[intJ].move(lyrBound,ElementPlacement.INSIDE);
-				lyrBound.visible = false;
-			} else {
-				newLayer.pathItems[intJ].remove();
+for (var i=0; i < lyrMain.layers.length; i++) {
+	// $.writeln(lyrMain.layers[i].name);
+	if (lyrMain.layers[i].groupItems.length) {
+		thisLayer = activeDocument.layers.add();
+		thisLayer.name = lyrMain.layers[i].name;
+		thisLayer.move(lyrMain, ElementPlacement.PLACEBEFORE);
+		lyrMain.layers[i].groupItems[0].move(thisLayer,ElementPlacement.INSIDE);
+		ungroup(thisLayer.groupItems[0]);
+		removeClip(thisLayer);
+	} else if (lyrMain.layers[i].layers.length) {
+		while (lyrMain.layers[i].layers.length) {
+			lyrMain.layers[i].layers[0].name = lyrMain.layers[i].name + '-' + lyrMain.layers[i].layers[0].name;
+			while (lyrMain.layers[i].layers[0].layers.length) {
+				while (lyrMain.layers[i].layers[0].layers[0].pageItems.length) {
+					lyrMain.layers[i].layers[0].layers[0].pageItems[0].move(lyrMain.layers[i].layers[0],ElementPlacement.INSIDE);
+				}
+				lyrMain.layers[i].layers[0].layers[0].remove();
 			}
+			while(lyrMain.layers[i].layers[0].groupItems.length) {
+				ungroup(lyrMain.layers[i].layers[0].groupItems[0]);
+			}
+			removeClip(lyrMain.layers[i].layers[0]);
+			lyrMain.layers[i].layers[0].move(lyrMain,ElementPlacement.PLACEBEFORE);
 		}
 	}
 }
